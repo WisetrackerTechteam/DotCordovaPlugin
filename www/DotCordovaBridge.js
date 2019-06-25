8464cc96628366b63195326d9156926384f620ec
kimkunsoo <mrcm@wisetracker.co.kr>
2019-06-25 14:05:25 +0900

    no message

--- www/DotCordovaBridge.js
+++ www/DotCordovaBridge.js
@@ -1,12 +1,20 @@
 exports.init = function(key, value, success, error) {
 	window.WISETRACKER_SDK_ENV_CODE = 2; 
 	// script dynamic loading 
-	try {
+	try { 
+		
+		cordova.exec(function() {console.log('success')},
+				function() {console.log('error')},
+				"DotCordovaBridge",
+				"initialization",
+				[]);
+		
 		var head= document.getElementsByTagName('head')[0];
 		var script= document.createElement('script');
 		script.type= 'text/javascript'; 
 		script.src = "plugins/kr.co.wisetracker/www/dop-native-sdk-inf.js";
-		head.appendChild(script);
+		head.appendChild(script);  
+		
 	} catch (e) {
 		console.log(e);
 	} 
