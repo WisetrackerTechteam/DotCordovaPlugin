1483384e1e3971908edc98034b497047615491e2
Woncheol Heo <>
2019-06-25 09:54:13 +0900

    ios 프레임워크 및 브릿지 추가

---
+++ src/ios/DOTBridge.m
@@ -0,0 +1,40 @@
+//
+//  Echo.m
+//  HelloWorld
+//
+//  Created by Woncheol Heo on 2019. 1. 23..
+//
+
+#import "DOTBridge.h"
+#import <DOT/DOT.h>
+@implementation WiseTrackerBridge
+
+
+- (void)echo:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+    
+    NSLog(@"echo: %@", echo);
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)initialization:(CDVInvokedUrlCommand*)command  {
+    
+    [DOT initialization];
+}
+
+- (void)setClick:(CDVInvokedUrlCommand*)command {
+    NSString *clickName = [command.arguments objectAtIndex:0];
+    Click *click = [[Click alloc] init];
+    click.clickName = clickName;
+    [DOT setClick:click];
+    
+}
+@end
