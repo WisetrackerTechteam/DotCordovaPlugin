865fff540680dc260ec0bdd1a2e16c44ba4ed45d
kimkunsoo <mrcm@wisetracker.co.kr>
2019-06-25 12:18:42 +0900

    no message

--- www/DotCordovaBridge.js
+++ www/DotCordovaBridge.js
@@ -1,15 +1,13 @@
-cordova.define("kr.co.wisetracker.DotCordovaBridge", function(require, exports, module) {
-	exports.init = function(key, value, success, error) {
-		window.WISETRACKER_SDK_ENV_CODE = 2; 
-		// script dynamic loading 
-		try {
-			var head= document.getElementsByTagName('head')[0];
-			var script= document.createElement('script');
-			script.type= 'text/javascript'; 
-			script.src = "./ko.kr.wisetracker/www/dop-native-sdk-inf.js";
-			head.appendChild(script);
-		} catch (e) {
-			console.log(e);
-		} 
+exports.init = function(key, value, success, error) {
+	window.WISETRACKER_SDK_ENV_CODE = 2; 
+	// script dynamic loading 
+	try {
+		var head= document.getElementsByTagName('head')[0];
+		var script= document.createElement('script');
+		script.type= 'text/javascript'; 
+		script.src = "pugins/kr.co.wisetracker/www/dop-native-sdk-inf.js";
+		head.appendChild(script);
+	} catch (e) {
+		console.log(e);
 	} 
 }
