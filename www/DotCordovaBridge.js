fa141e2e210be64b91b8802ef21c61a1d0a2dbc3
Woncheol Heo <>
2020-03-12 18:04:50 +0900

    js 파일 수정

--- www/DotCordovaBridge.js
+++ www/DotCordovaBridge.js
@@ -1,21 +1,26 @@
 exports.init = function(key, value, success, error) {
-	window.WISETRACKER_SDK_ENV_CODE = 2; 
-	// script dynamic loading 
-	try { 
-		
-		cordova.exec(function() {console.log('success')},
-				function() {console.log('error')},
-				"DotCordovaBridge",
-				"initialization",
-				[]);
-		
-		var head= document.getElementsByTagName('head')[0];
-		var script= document.createElement('script');
-		script.type= 'text/javascript'; 
-		script.src = "plugins/kr.co.wisetracker/www/dop-native-sdk-inf.js";
-		head.appendChild(script);  
-		
-	} catch (e) {
-		console.log(e);
-	} 
-}
+    try {
+        cordova.exec(function() {console.log('success')},
+                    function() {console.log('error')},
+                    "DotCordovaBridge",
+                    "initialization",
+                    []);
+     } catch (e) {
+         console.log(e);
+     }
+          
+  }
+  exports.injecting = function(key, value, success, error) {
+        window.WISETRACKER_SDK_ENV_CODE = 2;
+        try {
+           var head= document.getElementsByTagName('head')[0];
+           var script= document.createElement('script');
+           script.type= 'text/javascript';
+           script.src = cordova.file.applicationDirectory + "www/plugins/kr.co.wisetracker/www/dop-native-sdk-inf.js";
+           head.appendChild(script);
+        } catch (e) {
+           console.log('error');
+           console.log(e);
+        }
+        console.log('injecting:' + cordova.file.applicationDirectory );
+    }
